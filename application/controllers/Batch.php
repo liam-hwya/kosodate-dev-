3a8501cc7dfe27c8ecc3b829627bc5501e49e772
Htet Wai Yan Aung <waiyan@andfun.biz>
2022-02-15 22:45:26 +0630

    preparing rss data (part-3)

--- application/controllers/Batch.php
+++ application/controllers/Batch.php
@@ -15,8 +15,7 @@ class Batch extends CI_Controller {
 
         $this->load->helper('xml');
         $this->load->helper('text');
-        $this->load->model('d_media','d_media');
-        $this->load->model('d_manga','Manga_model');
+        $this->load->model('Manga_model','Manga_model');
 
         define('MAX_POST_LIMIT',30);
         define('POST_YEARS_AGO',1);
@@ -38,9 +37,9 @@ class Batch extends CI_Controller {
 
     public function prepareRSS()
     {   
-        $d_manga_col = $this->Manga_model->select_manga_rss();
-        
-            // Preparing XML data
+        $d_manga_col = $this->Manga_model->select_manga_for_rss();
+
+            // Preparing XML  Channel data
             $channel_data['title'] = "こそだてDAYS";
             $channel_data['link'] = "https://www.kosodatedays.com/";
             $channel_data['description'] = "こそだてDAYS（こそだてデイズ）は子育てママと作る0～6歳児ママのためのWebメディアです。ママ達の子育て体験談を無料で漫画化し、赤ちゃん期から入学までに必要な育児情報を配信しています。";
@@ -48,22 +47,141 @@ class Batch extends CI_Controller {
             $channel_data['language'] = "ja";
             $channel_data['copyright'] = "©2017 EvolvedInfo. All Rights Reserved.";
 
+            // Preparing XML item data
             $item_data = [];
+            $manga_id_start = true;
             foreach($d_manga_col as $key=>$manga_item){
                 $item_data[$key]['title'] = $manga_item['title'];
-                $item_data[$key]['link'] = $manga_item['img_url'];
+                $item_data[$key]['link'] = $manga_item['link'];
                 $item_data[$key]['guid'] = $manga_item['id'];
+                $item_data[$key]['tags_id'] = $manga_item['tags_id'];
                 $item_data[$key]['category'] = htmlspecialchars('<![CDATA[ママコマ漫画]]>');
                 $item_data[$key]['description'] = "こそだてDAYS（こそだてデイズ）は子育てママと作る0～6歳児ママのためのWebメディアです。ママ達の子育て体験談を無料で漫画化し、赤ちゃん期から入学までに必要な育児情報を配信しています。".$manga_item['author']."～「".$manga_item['title']."」をお楽しみください。";
                 $item_data[$key]['pubDate'] = nad_jp_date();
                 $item_data[$key]['modifiedDate'] = null;
-                $item_data[$key]['encoded'] = htmlspecialchars('<![CDATA[><h2>'.$manga_item['title'].'</h2><img src="'.$manga_item['img_url'].'"/>]]');
                 $item_data[$key]['delete'] = $manga_item['manga_deleted'];
                 $item_data[$key]['enclosure'] = $manga_item['img_url'];
                 $item_data[$key]['thumbnail'] = $manga_item['img_url'];
-                $item_data[$key]['related'] = "sample";
+                $item_data[$key]['relatedlink'] = null;
+            }
+            
+            // Select all image url for manga
+            for($manga_counter = 0; $manga_counter<count($item_data);$manga_counter++){
+
+                $manga_img_url_col = $this->Manga_model->select_manga_media($item_data[$manga_counter]['guid']);
+                $item_data[$manga_counter]['encoded'] = htmlspecialchars('<![CDATA[<h2>').$item_data[$manga_counter]['title'].htmlspecialchars('</h2>');
+                foreach($manga_img_url_col as $manga_img){
+                    $item_data[$manga_counter]['encoded'] .= htmlspecialchars('<img src="').$manga_img['img_url'].htmlspecialchars('"/>');
+                }
+                $item_data[$manga_counter]['encoded'] .= htmlspecialchars(']]>');
+
             }
 
+            // Select all related mangas for manga's tag 
+            foreach($d_manga_col as $key=>$item){
+                // $manga_id_tags[$key]['manga__id'] = $item['id'];
+                $manga_id_tags[$item['id']][$key]['manga_tags_id'] = $item['tags_id'];
+                $manga_id_tags[$item['id']][$key]['manga_tags_name'] = $item['tags_name'];
+            }
+
+            foreach($manga_id_tags as $manga_id=>$manga_item){
+              
+                foreach($manga_item as $manga){
+                
+                    switch ($manga['manga_tags_name']){
+                    
+                      case ONE_YEAR_OLD_TAG_AGE_NAME:
+                        $tags_names[] = ONE_YEAR_OLD_TAG_AGE;
+                        break;
+                      case TWO_YEAR_OLD_TAG_AGE_NAME:
+                        $tags_names[] = TWO_YEAR_OLD_TAG_AGE;
+                        break;
+                      case THREE_YEAR_OLD_TAG_AGE_NAME:
+                        $tags_names[] = THREE_YEAR_OLD_TAG_AGE;
+                        break;
+                      case FOUR_YEAR_OLD_TAG_AGE_NAME:
+                        $tags_names[] = FOUR_YEAR_OLD_TAG_AGE;
+                        break;
+                      case FIVE_YEAR_OLD_TAG_AGE_NAME:
+                        $tags_names[] = FIVE_YEAR_OLD_TAG_AGE;
+                        break;
+                      case SIX_YEAR_OLD_TAG_AGE_NAME:
+                        $tags_names[] = SIX_YEAR_OLD_TAG_AGE;
+                        break;
+                      default:
+                        $tags_names[] = null;
+                        break;
+                    
+                    }
+                }
+
+                asort($tags_names); // sorting ages from smallest to biggest
+
+                if(count($tags_names) == 1){ // Checkpoint for deciding manga limit
+
+                    $tags_condition = [
+                        [
+                            'manga_id' => $manga_id,
+                            'tags_id' => $tags_names[0],
+                            'manga_limit' => 3
+                        ]
+                    ];
+                }
+
+                if(count($tags_names) == 2){
+
+                    $tags_condition = [
+                        [
+                            'manga_id' => $manga_id,
+                            'tags_id' => $tags_names[0],
+                            'manga_limit' => 2
+                        ],
+                        [
+                            'manga_id' => $manga_id,
+                            'tags_id' => $tags_name[1],
+                            'manga_limit' => 1
+                        ]
+                    ];
+                }
+
+                if(count($tags_names) >= 3){
+
+                    $tags_condition = [
+                        [
+                            'manga_id' => $manga_id,
+                            'tags_id' => $tags_names[0],
+                            'manga_limit' => 1
+                        ],
+                        [
+                            'manga_id' => $manga_id,
+                            'tags_id' => $tags_names[1],
+                            'manga_limit' => 1
+                        ],
+                        [
+                            'manga_id' => $manga_id,
+                            'tags_id' => $tags_names[2],
+                            'manga_limit' => 1
+                        ]
+                    ];
+                }
+                $related_manga_tags_col = $this->Manga_model->select_related_manga_for_tags($tags_condition); //Manga collection of the related tag
+                
+            }
+            
+            // Preparing RSS item data relatedlink
+            for($manga_counter = 0; $manga_counter<count($item_data);$manga_counter++){
+
+                foreach($related_manga_tags_col as $each_item){
+
+                    foreach($each_item as $each_item_manga){
+                      
+                        $item_data[$manga_counter]['relatedlink'] .= htmlspecialchars('<relatedlink title="').$each_item_manga['manga_title'].htmlspecialchars('" link="').$each_item_manga['manga_url'].htmlspecialchars('" thumbnail="').$item_data[$manga_counter]['thumbnail'].htmlspecialchars('"/>');
+                    }
+                  
+                }
+            }
+            
+            // Preparing RSS-XML
             $xml=htmlspecialchars('<?xml version="1.0" encoding="UTF-8" ?>');
             $xml.=htmlspecialchars('<channel>');
             $xml.=htmlspecialchars('<title>'.$channel_data['title'].'</title>');
@@ -86,7 +204,7 @@ class Batch extends CI_Controller {
                 $xml.=htmlspecialchars('<delete>'.$item['delete'] .'</delete>');
                 $xml.=htmlspecialchars('<enclosure>'.$item['enclosure'] .'</enclosure>');
                 $xml.=htmlspecialchars('<thumbnail>'.$item['thumbnail'] .'</thumbnail>');
-                $xml.=htmlspecialchars('<related>'.$item['related'] .'</related>');
+                $xml.=htmlspecialchars('<related>'.$item['relatedlink'] .'</related>');
               $xml.=htmlspecialchars('</item>');
             }
             $xml.=htmlspecialchars('</channel>');
--- application/models/Manga_model.php
+++ application/models/Manga_model.php
@@ -1,14 +1,16 @@
 <?php
 
- class d_manga extends CI_MODEL {
+ class Manga_model extends CI_MODEL {
 
-    public function select_manga_rss() {
+    public function select_manga_for_rss() {
         
         $tags_id = $this->tags_id();
 
         $this->db->select('D_TAGS_MANGA.tags_id');
+        $this->db->select('D_TAGS.tags_name');
         $this->db->select('D_MANGA.manga_id AS id');
         $this->db->select('D_MANGA.manga_title AS title');
+        $this->db->select('D_MANGA.manga_url AS link');
         $this->db->select('D_MANGA.manga_intro AS intro');
         $this->db->select('D_MANGA.manga_date AS date');
         $this->db->select('D_MEDIA_1.media_url AS img_url');
@@ -18,9 +20,9 @@
         $this->db->select('D_MANGAKA.mangaka_nickname AS author');
         $this->db->from('D_MANGA');
         $this->db->join('D_MANGAKA','D_MANGA.mangaka_id = D_MANGAKA.mangaka_id','left');
-        $this->db->join('D_MANGA_MEDIA','D_MANGA.manga_id = D_MANGA_MEDIA.manga_id','left');
-        $this->db->join('D_MEDIA AS D_MEDIA_1','D_MANGA_MEDIA.media_id = D_MEDIA_1.media_id','left');
+        $this->db->join('D_MEDIA AS D_MEDIA_1','D_MANGA.manga_icon_media_id = D_MEDIA_1.media_id','left');
         $this->db->join('D_TAGS_MANGA','D_MANGA.manga_id = D_TAGS_MANGA.manga_id','left');
+        $this->db->join('D_TAGS','D_TAGS_MANGA.tags_id = D_TAGS.tags_id','left');
         $this->db->where('D_MANGA.manga_deleted',NO_DELETE_FLAG);
         $this->db->where('D_MANGA.manga_state_code',CONST_MANGA_STATE_CODE_PUBLIC);
         // $this->db->where('D_MANGA.manga_date >=',nad_jp_date());
@@ -28,13 +30,14 @@
         $this->db->where('D_MANGA.manga_date >=','2020-08-01 0:00:00');
         $this->db->where('D_MANGA.manga_date <=','2020-08-31 23:59:59');
         $this->db->where('D_MANGAKA.mangaka_state_code',CONST_MANGAKA_STATE_CODE_SHOW);
-        $this->db->where_in('D_TAGS_MANGA.tags_id',[4,6,14,17,24,27,50]);
+        $this->db->where_in('D_TAGS_MANGA.tags_id',$tags_id);
+        $this->db->group_by('D_TAGS_MANGA.tags_id');
+        $this->db->group_by('D_MANGA.manga_id');
         $this->db->order_by('D_TAGS_MANGA.tags_id','DESC');
         $this->db->order_by('D_MANGA.manga_date','DESC');
         $this->db->limit(CONST_RSS_MANGA_ITEM_NUM);
-        
+
         return $this->db->get()->result_array();
-        
     }
 
     public function tags_like_age($like_tags='歳') {
@@ -63,6 +66,46 @@
         return $tags_id;
     }
 
+    public function select_manga_media($manga_id) {
+        
+        $this->db->select('D_MEDIA_1.media_url AS img_url');
+        $this->db->from('D_MANGA');
+        $this->db->join('D_MANGA_MEDIA','D_MANGA.manga_id = D_MANGA_MEDIA.manga_id','left');
+        $this->db->join('D_MEDIA AS D_MEDIA_1','D_MANGA_MEDIA.media_id = D_MEDIA_1.media_id','left');
+        $this->db->where('D_MANGA.manga_id',$manga_id);
+
+        return $this->db->get()->result_array();
+    }
+
+    public function select_related_manga_for_tags($tags_condition) {
+
+        if($tags_condition){
+            foreach($tags_condition as $condition){
+                $this->db->select('D_MANGA.manga_id');
+                $this->db->select('D_MANGA.manga_title');
+                $this->db->select('D_MANGA.manga_url');
+                $this->db->from('D_MANGA');
+                $this->db->join('D_TAGS_MANGA','D_MANGA.manga_id = D_TAGS_MANGA.manga_id','left');
+                $this->db->join('D_TAGS','D_TAGS_MANGA.tags_id = D_TAGS.tags_id','left');
+                $this->db->where('D_TAGS.tags_id',$condition['tags_id']);
+                $this->db->order_by('D_TAGS_MANGA.tags_id','DESC');
+                $this->db->order_by('D_MANGA.manga_date','DESC');
+                $this->db->limit($condition['manga_limit']);
+
+                $result[$condition['manga_id']] = $this->db->get()->result_array();
+            }
+        }
+
+        return $result;
+
+    }
+
  }
 
 ?>
+
+
+
+
+
+
